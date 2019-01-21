48112941e78e4af75e1c38dd8bc150a408b6bdaa
ogesour <ogesour@gmail.com>
2019-01-20 21:53:28 -0800

    set up PORT/server

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
