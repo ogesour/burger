8a67eef6ba0f9bbc8df2978688e2e6b95610aa1d
ogesour <ogesour@gmail.com>
2019-01-26 22:33:52 -0800

    change

--- config/connection.js
+++ config/connection.js
@@ -13,10 +13,10 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'ogesor',
-        database: 'burgers_db'
+        host: 'umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'k0y1c3l12a094k9o',
+        password: 'oihtqk44rqarhg9w',
+        database: '	ezv11pjqao24jhcn'
     });
 };
 
